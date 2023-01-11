d933137e151f195eea7c943d5dd4583521253382
Eliot Du <108873919+EliotDu@users.noreply.github.com>
2023-01-11 22:15:15 +0800

    test

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
 require('dotenv').config();
 
-const line = require('@line/bot-sdk');
 const express = require('express');
+const line = require('@line/bot-sdk');
 const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 
 // create LINE SDK config from env variables
 const config = {
-    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-    channelSecret: process.env.CHANNEL_SECRET,
-}
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
 
-const configuration = new Configuration({
-      apiKey: process.env.OPENAI_API_KEY,
-    });
-const openai = new OpenAIApi(configuration);
-    
 // create LINE SDK client
 const client = new line.Client(config);
 
 // create Express app
 // about Express itself: https://expressjs.com/
-const app = express('');
+const app = express();
 
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
 app.post('/callback', line.middleware(config), (req, res) => {
-    Promise
-      .all(req.body.events.map(handleEvent))
-      .then((result) => res.json(result))
-      .catch((err) => {
-        console.error(err);
-        res.status(500).end();
-      });
-  });
-  
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
 
 // event handler
 async function handleEvent(event) {
@@ -45,15 +45,15 @@ async function handleEvent(event) {
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: event.message.text ,
+    max_tokens: 500,
   });
 
   // create a echoing text message
-  const echo = { type: 'text', text: completion.data.choices[0].text };
+  const echo = { type: 'text', text: completion.data.choices[0].text.trim() };
 
   // use reply API
   return client.replyMessage(event.replyToken, echo);
 }
-
 
 // listen on port
 const port = process.env.PORT || 3000;
